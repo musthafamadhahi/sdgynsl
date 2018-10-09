07999ad1be1cc8f7c5e8d1a8b940599b8887ff0e
shabthi <shabthij@gmail.com>
2018-10-09 21:24:41 +0530

    home page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,9 +1,23 @@
+@extends('layouts.app')
 <!doctype html>
 <html>
-    <head>
-        <title>@yield('title')</title>
-    </head>
-    <body>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-    </body>
+    <div class="container">
+        <a href="/"><img src="logo.jpg" ></a>
+        <nav class="nav">
+            <a class="nav-link active" href="#">About Us</a>
+            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">Events</a>
+            <a class="nav-link" href="#">Our Projects</a>
+            <a class="nav-link" href="#">Member Details</a>
+        </nav>
+    </div>
+
+
+
+
+
+
 </html>
